eaf2bb0fdaba0b47d0f1fb8e9d7cc7b786c35dd1
IndigoPurple <indigopurple@163.com>
2018-10-08 19:58:40 +0800

    Update getPosition.m

--- getPosition.m
+++ getPosition.m
@@ -1,16 +1,25 @@
-function  getPosition()
-
-clear;
+function getPositionInfo()
+%图像标定函数，用来标定图像中的head所在位置
+%标定完成后之间点击关闭图像显示框
 clc;
-im = imread('./GT_IMG/im_100.jpg');
-imshow(im);
+clear;
+im = imread('F:\Giga\primary_school_20180912_1_img\GT_IMG\im_50.jpg');
+g=imresize(im,0.25);
+imwrite(g,'F:\Giga2.0\CrowdCount-MCNN\data\original\shanghaitech\part_C_final\images\IMG_366.jpg')
+imshow(g);
 hold on;
-[x,y]=ginput;
-a=[x,y];
-b=size(x);
-b=b(1);
-image_info={};
-image_info.location=a;
-image_info.number=b;
-save GT_INFO/GT_IMG_100.mat image_info;
-disp("done");
+p=[];
+try
+   while true
+     a=ginput(1);
+     p=[p;a];
+     plot(a(1),a(2),'r*');
+   end     
+catch  
+     b=size(p);
+     b=b(1);
+     image_info={};
+     image_info.location=p;
+     image_info.number=b;
+     save F:\Giga2.0\CrowdCount-MCNN\data\original\shanghaitech\part_C_final\ground_truth\GT_IMG_366.mat image_info;
+end   
